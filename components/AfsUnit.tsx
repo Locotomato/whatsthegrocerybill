443113fe5545c0f1f2b196a44bb5a8aea34289b2
Michael Spitaleri <michaelaspitaleri@gmail.com>
2026-04-15 10:01:27 -0400

    fix: dangerouslySetInnerHTML to prevent Next.js script hoisting in AfsUnit

--- components/AfsUnit.tsx
+++ components/AfsUnit.tsx
@@ -1,13 +1,9 @@
 export default function AfsUnit() {
+  const scriptHtml = `<script src="https://locotomato.com/afs.js" data-partner="pub_rs2wayi1" data-campaign="cmp_db18b9c4" data-count="6" async></script>`
   return (
-    <div style={{ margin: '32px 0' }}>
-      <script
-        src="https://locotomato.com/afs.js"
-        data-partner="pub_rs2wayi1"
-        data-campaign="cmp_db18b9c4"
-        data-count="6"
-        async
-      />
-    </div>
+    <div
+      style={{ margin: '32px 0' }}
+      dangerouslySetInnerHTML={{ __html: scriptHtml }}
+    />
   )
 }
